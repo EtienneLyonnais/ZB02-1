7dbea92cb6d790450671223ff62597931a77b941
Etienne <1983429@cegepmontpetit.ca>
2023-08-28 19:05:26 -0400

    FCT Base de données en listes et liaisons
    Ajout de la classe contenant la base de données en listes et ajout de la liaison entre Zombie et ZombieType. Injection du Singleton

---
+++ ZombieParty/ZombieParty/Models/Zombie.cs
@@ -0,0 +1,11 @@
+﻿namespace ZombieParty.Models
+{
+    public class Zombie
+    {
+        public string Name { get; set; }
+        public int Point { get; set; }
+        public int ZombieTypeId { get; set; }
+        public ZombieType ZombieType { get; set; }
+
+    }
+}
